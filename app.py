7f40dc5677343f252105963322b47ab52f7cd74f
VaLik <67388625+valtru@users.noreply.github.com>
2023-06-18 18:05:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, request, Response
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
@@ -42,7 +43,7 @@ def notify():
         value = str(round(logs['event']['activity'][0]['value']))
 
         # create the text string
-        message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
+        message = f'*Date: {datetime.datetime.now().strftime('%Y-%m-%d/%H:%M:%S')}*\n*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
         bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
       
   return Response(status=200)
